3ac8a11f98312c47dee43223c99ad88a137ae702
Noxist <noxist@tuta.io>
2025-12-21 01:20:23 +0700

    Edit version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -51,7 +51,7 @@ android {
         minSdk = 26
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.9"
     }
     packaging {
         resources {
